e2e61bd1ae5a1372481586cc97854e6a6df4efd3
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-04-12 21:57:07 -0400

    audio playlist implemented

--- client/src/components/MusicPlayer/MusicPlayer.js
+++ client/src/components/MusicPlayer/MusicPlayer.js
@@ -1,92 +1,120 @@
-import React, {useState, useEffect, useRef}from 'react'
-import "./musicplayer.css"
+import React, { useState, useEffect, useRef } from "react";
+import "./musicplayer.css";
 
 /**
-* @author
-* @function MusicPlayer
-**/
-
+ * @author
+ * @function MusicPlayer
+ **/
 
 const MusicPlayer = (props) => {
   const player = useRef();
-  const [currentSong, setcurrentSong]= useState(null);
-  const [isPlaying, setisPlaying]= useState(false);
-  const play = ()=>{
-    if(isPlaying===false){
-      player.current.play()
-      setisPlaying(true)
-    }else{
+  const [isPlaying, setisPlaying] = useState(false);
+
+  // Function to control state if song is playing or paused
+  const play = () => {
+    if (isPlaying === false) {
+      player.current.play();
+      setisPlaying(true);
+    } else {
       player.current.pause();
-      setisPlaying(false)
+      setisPlaying(false);
     }
-   
-   
-    
-  }
+  };
 
-  const stop = () =>{
+  // stop song from playing
+  const stop = () => {
     player.current.pause();
-  }
+  };
 
-  const renderPlay = () =>{
-    if(isPlaying=== true){
+  //Update play/pause button based off of state
+  const renderPlay = () => {
+    if (isPlaying === true) {
       return (
-        <button className="audioBut"onClick={play}><i className="fa fa-pause" /></button>
-      )
-    }else{
+        <button className="audioBut" onClick={play}>
+          <i className="fa fa-pause" />
+        </button>
+      );
+    } else {
       return (
-      <button className="audioBut"onClick={play}><i className="fa fa-play" /></button>
-      )
+        <button className="audioBut" onClick={play}>
+          <i className="fa fa-play" />
+        </button>
+      );
     }
-  }
-  useEffect(()=> {
-    
+  };
 
-    console.log(player.current.duration)
-  
-  },[])
-  
-  
-  return(
+  return (
     <div>
-    <div>
-      <audio ref={player} preload="auto" controls>
-        <source src={require("./Recording.m4a")} type ="audio/mpeg"></source>
+      <div>
+        <audio ref={player} preload="auto" controls>
+          <source src={require("./Recording.m4a")} type="audio/mpeg"></source>
         </audio>
-         
+      </div>
+      <div className="glow container">
+        <div className="text-container">
+          <span className="text">Song Name</span>
+          <br />
+          <span className="text">Artist</span>
+          <br />
+          <div className="playback_controls">
+            <button className="audioBut" onClick="skip('back')">
+              <i className="fa fa-fast-backward" />
+            </button>
+            {renderPlay()}
+            <button className="audioBut" onClick={stop}>
+              <i className="fa fa-stop" />
+            </button>
+            <button className="audioBut" onClick="skip('fwd')">
+              <i className="fa fa-fast-forward" />
+            </button>
+          </div>
+          <br />
+          <div id="seekbar">
+            <input type="range" oninput="f" id="seek" defaultValue={0} />
+          </div>
+          <br />
+          <div className="volume_controls">
+            <button className="audioBut" id="mute" onClick="mute()">
+              <i className="fa fa-volume-up" />
+            </button>
+            <input
+              type="range"
+              id="volume"
+              oninput="setVolume(this.value)"
+              min={0}
+              max={1}
+              step="0.01"
+              defaultValue={1}
+            />
+          </div>
+        </div>
+      </div>
 
+      {/* playlist */}
+      <div className="play-list">
+        <div className="container  ">
+          <div className="row ">
+            <div className="col-12 col-sm-8 col-lg-5">
+              <h6 className="text-muted">Album/Playlist Name</h6>
+              <ul className="list-group">
+                <li className="list-group-item d-flex align-items-center">
+                  <div className="image-parent">
+                    <img
+                      src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/things_fall_apart.jpg"
+                      className="img-fluid"
+                      alt="things"
+                    />
+                  </div>
+                  1. Song Name{" "}
+                  <div className="play-listitem">{renderPlay()}</div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  <div className="glow container">
-    <div className="text-container">
-      <span className="text">{currentSong}</span>
-      <br />
-      <span className="text">Coding and Stuff</span>
-      <br />
-      <div className="playback_controls">
-        <button className="audioBut"onClick="skip('back')"><i className="fa fa-fast-backward" /></button>
-       {renderPlay()}
-        <button className="audioBut"onClick={stop}><i className="fa fa-stop" /></button>
-        <button className="audioBut"onClick="skip('fwd')"><i className="fa fa-fast-forward" /></button>
-      </div>
-      <br />
-      <div id="seekbar">
-        <input type="range" oninput="f" id="seek" defaultValue={0}/>
-      </div>
-      <br />
-      <div className="volume_controls">
-        <button className="audioBut" id="mute" onClick="mute()"><i className="fa fa-volume-up" /></button>
-        <input type="range" id="volume" oninput="setVolume(this.value)" min={0} max={1} step="0.01" defaultValue={1} />
-      </div>
-    </div>
-  </div>
-</div>
+  );
+};
 
-   )
-
- } 
-
-//  <button onclick={ref("player").play()}>Play</button>
-//  <button onclick={"document.getElementById('player').pause()"}>Pause</button>
-//  <button onclick="document.getElementById('player').muted=!document.getElementById('player').muted">Mute/ Unmute</button>
-
-export default MusicPlayer
+export default MusicPlayer;
