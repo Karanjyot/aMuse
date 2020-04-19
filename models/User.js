7a4b8fbff7ac6bc12b10c73228795b8839ce96ce
nazkin <nazar.kinash93@gmail.com>
2020-04-19 03:23:59 -0400

    model work

--- models/User.js
+++ models/User.js
@@ -29,3 +29,22 @@ userSchema.plugin(require('mongoose-bcrypt'))
 const User = mongoose.model("users", userSchema)
 
 module.exports = User;
+// const mongoose = require('mongoose');
+
+// const CommentSchema = new mongoose.Schema({
+//     text: {
+//         type: String
+//     },
+//     datePosted:{
+//         type: Date,
+//         default: Date.now()
+//     },
+//     authorID:{
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'users'  
+//     }
+// });
+
+// const Song = mongoose.model('Comment', CommentSchema);
+
+// module.exports = Song;
