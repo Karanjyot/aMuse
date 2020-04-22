8097a01042fd35bd00bf554b97d8a53a0dc8dd3d
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-04-21 22:06:47 -0400

    fix error with db

--- client/src/components/LibraryDisplay/LibraryDisplay.js
+++ client/src/components/LibraryDisplay/LibraryDisplay.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./librarydisplay.css";
-import axios from "axios"
+import axios from "axios";
 import MusicPlayerHome from "../../components/MusicPlayerHome/MusicPlayerHome";
 /**
  * @author
@@ -8,81 +8,78 @@ import MusicPlayerHome from "../../components/MusicPlayerHome/MusicPlayerHome";
  **/
 
 const LibraryDisplay = (props) => {
-const [Library, setCurrentLibrary] = useState([])
-    useEffect(()=> {
-        axios.get(`/api/current_user/data`)
-          .then(res=> {
-            console.log(res.data.account.library);
-            setCurrentLibrary(res.data.account.library);
-          });
-      }, []);
+  const [Library, setCurrentLibrary] = useState([]);
 
-  
-      var allSongs = Library.map((song, index) => {
-       
-          return (
-            <li key={1} className="card mb-3">
-              <div className="row no-gutters">
-                <div className="col-md-4 ">
-                  {
-                    <img
-                      src={song.albumPhoto}
-                      className="card-img album-img"
-                      alt="..."
-                    />
-                  }
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <a href="/audioplayer">
-                      <h5 className="card-title">
-                        {song.name.replace(/\.[^/.]+$/, "")}
-                      </h5>
-                    </a>
-                    {/* <a href="/audioplayer">
+  // retrieve library of current user
+  useEffect(() => {
+    axios.get(`/api/current_user/data`).then((res) => {
+      console.log(res.data.account.library);
+      setCurrentLibrary(res.data.account.library);
+    });
+  }, []);
+
+
+  var allSongs = Library.map((song, index) => {
+    return (
+      <li key={`${song._id}1`} className="card mb-3">
+        <div className="row no-gutters">
+          <div className="col-md-4 ">
+            {
+              <img
+                src={song.albumPhoto}
+                className="card-img album-img"
+                alt="..."
+              />
+            }
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <a href="/audioplayer">
+                <h5 className="card-title">
+                  {song.name.replace(/\.[^/.]+$/, "")}
+                </h5>
+              </a>
+              {/* <a href="/audioplayer">
                       <p className="card-text">{account.artist_nickname}</p>
                     </a> */}
-    
-                    {/* <audio controls ref={player} src={song.downloadURL} />
-                    {renderPlay()} */}
-                    <MusicPlayerHome
-                      songName={song.name}
-                      downloadURL={song.downloadURL}
-                      songID={song._id}
-                    />
-                    
-                  </div>
-                </div>
-              </div>
-            </li>
-          );
-       
-      });   
 
-///api/librarysong/:id
+              {/* <audio controls ref={player} src={song.downloadURL} />
+                    {renderPlay()} */}
+              <MusicPlayerHome
+                songName={song.name}
+                downloadURL={song.downloadURL}
+                songID={song._id}
+              />
+            </div>
+          </div>
+        </div>
+      </li>
+    );
+  });
+
   return (
     <div className="App jumbotron">
-    <h1 className="genre">Library</h1>
+      <h1 className="genre">Library</h1>
 
-    <div className="container">
-      <div className="active-pink-3 active-pink-4">
-        <input
-          className="form-control"
-          id="listSearch1"
-          type="text"
-          placeholder="Search for a song"
-        />
-        <br />
-        <div className="jb">
-          <div className="row">
-            <ul className="list-group" id="myList1">
-              {allSongs}
-            </ul>
+      <div className="container">
+        <div className="active-pink-3 active-pink-4">
+          <input
+            className="form-control"
+            id="listSearch1"
+            type="text"
+            placeholder="Search for a song"
+          />
+          <br />
+          <div className="jb">
+            <div className="row">
+              <ul className="list-group" id="myList1">
+                {allSongs}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
