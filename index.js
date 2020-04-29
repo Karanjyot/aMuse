8d9a5fdbba70aa62750fd491a2bc46ce5c38621c
nazkin <nazar.kinash93@gmail.com>
2020-04-29 18:19:44 -0400

    pre-deploy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const passport = require ("passport");
 const keys = require("./config/keys");
 const cors = require("cors");
+const path = require('path');
 
 require("./models/User");
 require("./services/passport");
@@ -28,8 +29,14 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 authRoutes(app);
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 const PORT = process.env.PORT || 5000;
 // listen to port 
