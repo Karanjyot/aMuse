d8cc3e95d4622cc0d1e8804bce49a40ecefa93cd
nazkin <nazar.kinash93@gmail.com>
2020-04-29 17:52:50 -0400

    Fixed the signup issue

--- client/src/Firebase/index.js
+++ client/src/Firebase/index.js
@@ -2,22 +2,22 @@ import firebase from 'firebase/app';
 import "firebase/storage";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBfcdxQgnw65W-0NkmgQfrSxOjdfXXgo8c",
-    authDomain: "amuse-f8ada.firebaseapp.com",
-    databaseURL: "https://amuse-f8ada.firebaseio.com",
-    projectId: "amuse-f8ada",
-    storageBucket: "amuse-f8ada.appspot.com",
-    messagingSenderId: "823202468853",
-    appId: "1:823202468853:web:da0342efd847077c1a86bc",
-    measurementId: "G-03F689M0ST"
-    // apiKey: process.env.REACT_APP_API_KEY,
-    // authDomain:process.env.REACT_APP_AUTH_DOMAIN,
-    // databaseURL: process.env.REACT_APP_DB_URL,
-    // projectId: process.env.REACT_APP_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
-    // appId: process.env.REACT_APP_APP_ID,
-    // measurementId: process.env.REACT_APP_MEASURENEMEN,
+    // apiKey: "AIzaSyBfcdxQgnw65W-0NkmgQfrSxOjdfXXgo8c",
+    // authDomain: "amuse-f8ada.firebaseapp.com",
+    // databaseURL: "https://amuse-f8ada.firebaseio.com",
+    // projectId: "amuse-f8ada",
+    // storageBucket: "amuse-f8ada.appspot.com",
+    // messagingSenderId: "823202468853",
+    // appId: "1:823202468853:web:da0342efd847077c1a86bc",
+    // measurementId: "G-03F689M0ST"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DB_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURENEMEN,
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -18,6 +18,7 @@ const Login = () => {
     axios
       .post("/api/local/login", obj)
       .then((res) => {
+        console.log(res);
         history.push("/home");
       })
       .catch((err) => console.log(err));
